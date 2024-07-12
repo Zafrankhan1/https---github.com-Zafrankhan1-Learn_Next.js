21039a525676c8c8bbe7cbd7303c9c83b6d3c1f6
Zafran Khan <zafrankhattak480@gmail.com>
2024-07-13 00:11:02 +0500

    first commit

---
+++ src/app/step00_helloworld_typescript/_index.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/app";
+
+function MyApp({Component , pageProps}:AppProps) {
+    return (
+<div>
+    <Component {...pageProps} />
+</div>
+    )
+};
+
+export default MyApp;
---
+++ src/app/step00_helloworld_typescript/page.tsx
@@ -0,0 +1,21 @@
+function helloWorld() {
+  return (
+    <div>
+        HelloWorld
+<div>
+    <Home />
+</div>
+    </div>
+  )
+}
+
+export default helloWorld;
+
+import { NextPage } from "next";
+const Home: NextPage = () => {
+return (
+    <div>
+        hello
+    </div>
+)
+};
